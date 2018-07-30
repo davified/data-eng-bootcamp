4dd7c87e4206de3e981f41205ab2e2fda94aa696
davified <davified@gmail.com>
2018-07-30 14:39:51 +0800

    Update setup script to install docker and get sequenceiq/hadoop-docker image

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 ### Setup
 - Fork and clone repo
-- Start docker containers in detached (i.e. background) mode: `docker-compose up -d`
-- Start bash shell in containers:
-  - `docker attach spark`
+- Run `./setup.sh`
+
+### Lab: HDFS
+- Run bash shell in container: `docker run -it sequenceiq/hadoop-docker:2.7.1 /etc/bootstrap.sh -bash`
+- `cd $HADOOP_PREFIX`
+- execute commands listed in lab!
+
+
+TODOs
+- add an option/step to mount a data volume with avro files or parquet files
--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-# install docker and docker-compose?
+which docker 
+if [ $? -ne 0 ]; then
+  echo "[INFO] Installing docker..."
+  brew cask install docker
+fi
 
-echo "[INFO] Downloading and installing Spark"
+echo "[INFO] Pulling Apache Hadoop 2.7.0 Docker image..."
+docker pull sequenceiq/hadoop-docker:2.7.1
 
+echo "[INFO] Downloading and installing Spark..."
 if [[ ! -f spark-2.3.1-bin-hadoop2.7.tgz ]]; then
   wget http://www-eu.apache.org/dist/spark/spark-2.3.1/spark-2.3.1-bin-hadoop2.7.tgz 
 fi
@@ -14,15 +20,13 @@ fi
 
 echo "[INFO] Spark installed."
 
-
-
-virtual_environment_name=.venv_data_eng_bootcamp
 which python3
 if [ $? -ne 0 ]; then
   echo "[INFO] Installing python 3"
   brew install python3
 fi
 
+virtual_environment_name=.venv_data_eng_bootcamp
 if [ ! -d ${virtual_environment_name} ]; then
   echo "[INFO] Creating virtual env folder in current directory"
   python3 -m venv ${virtual_environment_name}
@@ -33,11 +37,14 @@ source ${virtual_environment_name}/bin/activate
 pip3 install jupyter
 python3 -m ipykernel install --user --name ${virtual_environment_name} --display-name "${virtual_environment_name}"
 
-
-echo '[INFO] Run the following commands'
-echo "source ${virtual_environment_name}/bin/activate"
-echo 'export SPARK_HOME="$(pwd)/spark-2.3.1-bin-hadoop2.7"'
-
+echo '============================================='
+echo '[INFO] Done!'
+echo '[INFO] ======= Commands for HDFS Lab ======='
+echo '[INFO] docker run -it sequenceiq/hadoop-docker:2.7.1 /etc/bootstrap.sh -bash'
+echo ''
+echo '[INFO] ======= Commands for Spark Lab ======'
+echo "[INFO] To activate virtual env: source ${virtual_environment_name}/bin/activate"
+echo "[INFO] Define spark home: export SPARK_HOME=spark-2.3.1-bin-hadoop2.7"
 echo '[INFO] To start spark in spark shell, run: spark-2.3.1-bin-hadoop2.7/bin/pyspark --master local'
 echo '[INFO] To start spark in jupyter environment, run: PYSPARK_DRIVER_PYTHON=jupyter PYSPARK_DRIVER_PYTHON_OPTS=notebook spark-2.3.1-bin-hadoop2.7/bin/pyspark --master local'
 echo '[INFO] To deactivate the virtual environment, run: deactivate'
