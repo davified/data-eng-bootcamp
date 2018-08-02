53d027aec6d582346ff57132282e00b4bafe093e
davified <davified@gmail.com>
2018-08-02 11:21:14 +0800

    Update setup script

--- setup.sh
+++ setup.sh
@@ -21,12 +21,12 @@ echo "[INFO] Pulling Apache Hadoop 2.7.0 Docker image..."
 docker pull sequenceiq/hadoop-docker:2.7.1
 
 echo "[INFO] Downloading and installing Spark..."
-if [[ ! -f spark-2.3.1-bin-hadoop2.7.tgz ]]; then
-  wget http://www-eu.apache.org/dist/spark/spark-2.3.1/spark-2.3.1-bin-hadoop2.7.tgz 
+if [[ ! -f ./opt/spark-2.3.1-bin-hadoop2.7.tgz ]]; then
+  wget -P ./opt http://www-eu.apache.org/dist/spark/spark-2.3.1/spark-2.3.1-bin-hadoop2.7.tgz 
 fi
 
 if [[ ! -d spark-2.3.1-bin-hadoop2.7 ]]; then
-  tar xvf spark-2.3.1-bin-hadoop2.7.tgz
+  tar xvf ./opt/spark-2.3.1-bin-hadoop2.7.tgz -C ..
 fi
 
 echo "[INFO] Spark installed."
@@ -51,11 +51,13 @@ pip3 install numpy
 python3 -m ipykernel install --user --name ${virtual_environment_name} --display-name "${virtual_environment_name}"
 
 echo '============================================='
-echo '[INFO] Done!'
-echo '[INFO] ======= Commands for HDFS Lab ======='
-echo '[INFO] docker run -it sequenceiq/hadoop-docker:2.7.1 /etc/bootstrap.sh -bash'
+echo '[INFO] Setup complete!'
+echo '[INFO] ============== Get data =============='
+echo '[INFO] To get data for the labs, run: ./get_data.sh'
+echo '[INFO] ======= Commands for HDFS Lab ========'
+echo '[INFO] docker run -v $(pwd)/data:/usr/local/data/ -it sequenceiq/hadoop-docker:2.7.1 /etc/bootstrap.sh -bash'
 echo ''
-echo '[INFO] ======= Commands for Spark Lab ======'
+echo '[INFO] ======= Commands for Spark Lab ======='
 echo "[INFO] To activate virtual env: source ${virtual_environment_name}/bin/activate"
 echo "[INFO] Define spark home: export SPARK_HOME=spark-2.3.1-bin-hadoop2.7"
 echo '[INFO] To start spark in spark shell, run: spark-2.3.1-bin-hadoop2.7/bin/pyspark --master local'
